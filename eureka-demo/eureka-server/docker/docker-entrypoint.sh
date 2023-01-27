d40558fbd8dba971cc1bbeef03b875d424ae0f3d
mcmin001 <34086645+mcmin001@users.noreply.github.com>
2023-01-27 23:31:37 +0800

    Update docker-entrypoint.sh

--- eureka-demo/eureka-server/docker/docker-entrypoint.sh
+++ eureka-demo/eureka-server/docker/docker-entrypoint.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+APP_SERVER_HOME=${APP_SERVER_HOME:-"/data/project/eureka-server"}
+APP_SERVER_CONFIG_HOME=${APP_SERVER_CONFIG_HOME:-"/data/project/eureka-server/config"}
+APP=eureka-server-1.0-SNAPSHOT.jar
+
+set -e
+
+exec java -jar ${APP_SERVER_HOME}/${APP} --spring.config.location=${APP_SERVER_CONFIG_HOME}/application.yml
+
+exec "$@"
