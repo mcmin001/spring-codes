668747f657ea7893a3c8ae150ecadd9d79508c68
mcmin001 <34086645+mcmin001@users.noreply.github.com>
2023-01-27 22:17:34 +0800

    提交docker-entrypoint.sh

--- eureka-demo/server-provider/docker/docker-entrypoint.sh
+++ eureka-demo/server-provider/docker/docker-entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+APP_SERVER_HOME=${APP_SERVER_HOME:-"/data/project/server-provider"}
+
+set -e
+
+exec java -jar ${APP_SERVER_HOME}/server-provider-1.0-SNAPSHOT.jar --spring.config.location=../config/application.yml
+
+exec "$@"
