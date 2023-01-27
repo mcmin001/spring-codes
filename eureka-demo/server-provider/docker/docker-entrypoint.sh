d277c4a2420ad1dbdd1ea5c3ce7ed2e404eb0a23
mcmin001 <34086645+mcmin001@users.noreply.github.com>
2023-01-27 23:22:12 +0800

    修改

--- eureka-demo/server-provider/docker/docker-entrypoint.sh
+++ eureka-demo/server-provider/docker/docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 APP_SERVER_HOME=${APP_SERVER_HOME:-"/data/project/server-provider"}
+APP_SERVER_CONFIG_HOME=${APP_SERVER_CONFIG_HOME:-"/data/project/server-provider/config"}
 
 set -e
 
-exec java -jar ${APP_SERVER_HOME}/server-provider-1.0-SNAPSHOT.jar --spring.config.location=../config/application.yml
+exec java -jar ${APP_SERVER_HOME}/server-provider-1.0-SNAPSHOT.jar --spring.config.location=${APP_SERVER_CONFIG_HOME}/application.yml
 
 exec "$@"
